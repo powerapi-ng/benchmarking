2c59102da7016b70f3265152cb7bad0a072253d1
daniel <daniel.romero@inria.fr>
2024-10-10 09:32:18 +0200

    feat: Add Intel Nuc 9 generation scripts for hwpc sensor tests

---
+++ powerapi-machines/intel/nuc-9-generation/hwpc-sensor/scripts/matrix-10-min-1-instance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run --rm -d --net=host --privileged --pid=host --name "hwpc-sensor" -v /sys:/sys -v /var/lib/docker/containers:/var/lib/docker/containers:ro -v /tmp/powerapi-sensor-reporting:/reporting -v $(pwd):/srv -v $(pwd)/result-matrix-10-min-1-instance-$(date +"%FT%H%M"):/result-matrix-10-min-1-instance-$(date +"%FT%H%M") powerapi/hwpc-sensor:latest -n "$(hostname -f)" -U /result-matrix-10-min-1-instance-$(date +"%FT%H%M") -s "rapl" -o -e "RAPL_ENERGY_PKG" -s "msr" -e "TSC" -e "APERF" -e "MPERF" -c "core" -e "CPU_CLK_THREAD_UNHALTED:REF_P" -e "CPU_CLK_THREAD_UNHALTED:THREAD_P" -e "LLC_MISSES" -e "INSTRUCTIONS_RETIRED" -p "/sys/fs/cgroup/perf_event"
+
+for i in {1..20}
+do
+	docker run --rm --name "matrix-10-min-1-instance" ghcr.io/colinianking/stress-ng --matrix 1 -t 10m --times >> result-matrix-10-min-1-instance-$(date +"%FT%H%M").txt
+done
+
+docker stop hwpc-sensor
