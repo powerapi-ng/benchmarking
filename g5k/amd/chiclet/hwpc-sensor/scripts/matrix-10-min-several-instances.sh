381f3151dd03b46d2f4d3c3cd3be2721ec0c31e3
daniel <daniel.romero@inria.fr>
2024-10-10 09:27:44 +0200

    feat: Add AMD Chiclet scripts for hwpc sensor tests

---
+++ g5k/amd/chiclet/hwpc-sensor/scripts/matrix-10-min-several-instances.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+g5k-setup-docker -t
+
+mkdir result-matrix-10-min-several-instances-$(date +"%FT%H%M")
+
+docker run --rm -d --net=host --privileged --pid=host --name "hwpc-sensor" -v /sys:/sys -v /var/lib/docker/containers:/var/lib/docker/containers:ro -v /tmp/powerapi-sensor-reporting:/reporting -v $(pwd):/srv -v $(pwd)/result-matrix-10-min-several-instances-$(date +"%FT%H%M"):/result-matrix-10-min-several-instances-$(date +"%FT%H%M") powerapi/hwpc-sensor:latest -n "$(hostname -f)" -U /result-matrix-10-min-several-instances-$(date +"%FT%H%M") -s "rapl" -o -e "RAPL_ENERGY_PKG" -s "msr" -e "TSC" -e "APERF" -e "MPERF" -c "core" -e "CYCLES_NOT_IN_HALT" -e "RETIRED_INSTRUCTIONS" -e "RETIRED_UOPS" -p "/sys/fs/cgroup/perf_event"
+
+
+for i in {1..20}
+do
+	docker run --rm --name "matrix-10-min-several-instances" ghcr.io/colinianking/stress-ng --matrix 0 -t 10m --times >> result-matrix-10-min-several-instances-$(date +"%FT%H%M").txt
+done
+
+docker stop hwpc-sensor
