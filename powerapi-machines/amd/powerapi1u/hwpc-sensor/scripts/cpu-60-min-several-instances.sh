cbf2094635862f308d7bde2ec45356ca47ab97d2
daniel <daniel.romero@inria.fr>
2024-10-10 09:30:50 +0200

    feat: Add AMD Powerapi1u scripts for hwpc sensor tests

---
+++ powerapi-machines/amd/powerapi1u/hwpc-sensor/scripts/cpu-60-min-several-instances.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run --rm -d --net=host --privileged --pid=host --name "hwpc-sensor" -v /sys:/sys -v /var/lib/docker/containers:/var/lib/docker/containers:ro -v /tmp/powerapi-sensor-reporting:/reporting -v $(pwd):/srv -v $(pwd)/result-cpu-60-min-several-instances-$(date +"%FT%H%M"):/result-cpu-60-min-several-instances-$(date +"%FT%H%M") powerapi/hwpc-sensor:latest -n "$(hostname -f)" -U /result-cpu-60-min-several-instances-$(date +"%FT%H%M") -s "rapl" -o -e "RAPL_ENERGY_PKG" -s "msr" -e "TSC" -e "APERF" -e "MPERF" -c "core" -e "CYCLES_NOT_IN_HALT" -e "RETIRED_INSTRUCTIONS" -e "RETIRED_UOPS" -p "/sys/fs/cgroup/perf_event"
+
+for i in {1..20}
+do
+	docker run --rm --name "cpu-60-min-several-instances" ghcr.io/colinianking/stress-ng --cpu 0 --cpu-method all -t 60m --times >> result-cpu-60-min-several-instances-$(date +"%FT%H%M").txt
+done
+
+docker stop hwpc-sensor
---
+++ powerapi-machines/amd/powerapi1u/hwpc-sensor/scripts/matrix-60-min-several-instances.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run --rm -d --net=host --privileged --pid=host --name "hwpc-sensor" -v /sys:/sys -v /var/lib/docker/containers:/var/lib/docker/containers:ro -v /tmp/powerapi-sensor-reporting:/reporting -v $(pwd):/srv -v $(pwd)/result-matrix-60-min-several-instances-$(date +"%FT%H%M"):/result-matrix-60-min-several-instances-$(date +"%FT%H%M") powerapi/hwpc-sensor:latest -n "$(hostname -f)" -U /result-matrix-60-min-several-instances-$(date +"%FT%H%M") -s "rapl" -o -e "RAPL_ENERGY_PKG" -s "msr" -e "TSC" -e "APERF" -e "MPERF" -c "core" -e "CYCLES_NOT_IN_HALT" -e "RETIRED_INSTRUCTIONS" -e "RETIRED_UOPS" -p "/sys/fs/cgroup/perf_event"
+
+for i in {1..20}
+do
+	docker run --rm --name "matrix-60-min-several-instances" ghcr.io/colinianking/stress-ng --matrix 0 -t 60m --times >> result-matrix-60-min-several-instances-$(date +"%FT%H%M").txt
+done
+
+docker stop hwpc-sensor
