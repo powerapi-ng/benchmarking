4cc8574481da4cb1d0ae3658a78cca77ef4ea442
Inkedstinct <nathan.leblond@inria.fr>
2024-10-18 16:57:37 +0200

    feat(inventory): Add inventory lib functions

--- libs/inventory.sh
+++ libs/inventory.sh
@@ -1,85 +1,88 @@
 #!/bin/bash
-set +x
-set -euo pipefail
-IFS=$'\n\t'
+function get_api_call {
 
-# Example complete url : https://api.grid5000.fr/stable/sites/lille/clusters/chiclet/nodes/chiclet-1.json?pretty
-BASE_API_URI="https://api.grid5000.fr/stable/"
-SITES_ENDPOINT="sites"
-CLUSTERS_ENDPOINT="clusters"
-NODES_ENDPOINT="nodes"
-NODE_ENDPOINT=""
-NODE_ENDPOINT_SUFFIX=".json?pretty"
-INVENTORY_DIRECTORY="./inventories.d"
+    local ENDPOINT="$1"
+    logThis "inventory" "Scrapping ${ENDPOINT}" "INFO"  || true
 
-# SSH Proxy used to hit API
-PROXY_SERVER=${1:-"g5k"}
-
-
-
-### LOGGING
-APP_NAME="inventory"
-SCRIPT_LOGGING_LEVEL="${LOG_LEVEL:-"DEBUG"}"
-source libs/logging.sh
-function inventoryLogThis {
-    logThis "inventory" "$1" "$2"
-}
-
-function api_call {
-
-    local ENDPOINT=$1
-    inventoryLogThis "Scrapping ${ENDPOINT}" "INFO"
-
-    ssh "${PROXY_SERVER}" curl -s "$ENDPOINT"
+    curl -s -u "$G5K_USERNAME:$G5K_PASSWORD" "$ENDPOINT"
 
 }
 
-function main {
-    inventoryLogThis "Starting Benchmarks !" "INFO"
-    
-    inventoryLogThis "Remove old inventory files to update" "INFO"
+function generate_inventory {
+    if [[ -z "$G5K_USERNAME" || -z "$G5K_PASSWORD" ]]; then
+        echo "Missing $G5K_USERNAME and/or $G5K_PASSWORD. Both are needed for API authentication"
+        exit 1
+    fi
+    # Example complete url : https://api.grid5000.fr/stable/sites/lille/clusters/chiclet/nodes/chiclet-1.json?pretty
+    local BASE_API_URI="https://api.grid5000.fr/stable/"
+    local SITES_ENDPOINT="sites"
+    local CLUSTERS_ENDPOINT="clusters"
+    local NODES_ENDPOINT="nodes"
+    local NODE_ENDPOINT=""
+    local NODE_ENDPOINT_SUFFIX=".json?pretty"
+    local INVENTORY_DIRECTORY="$1"
+    local SCRIPT_LOGGING_LEVEL="DEBUG"
+
+    logThis "inventory" "Remove old inventory files to update" "INFO" || true
     find ./inventories.d/ -type f -mtime +7 -exec rm {} \;
-
-    SITES="$(api_call ${BASE_API_URI}${SITES_ENDPOINT})"
+    
+    SITES="$(get_api_call "${BASE_API_URI}${SITES_ENDPOINT}")"
     mapfile -t SITES_UID < <(echo "${SITES}" | yq '.items[] | .uid')
     
     for SITE_UID in "${SITES_UID[@]}"; do
-        inventoryLogThis "Processing site: ${SITE_UID}" "DEBUG"
-        mkdir -p $INVENTORY_DIRECTORY/$SITE_UID/
+        logThis "inventory" "Processing site: ${SITE_UID}" "DEBUG" || true
+        mkdir -p "$INVENTORY_DIRECTORY/$SITE_UID/"
         
         SITE_API_URI="${BASE_API_URI}${SITES_ENDPOINT}/${SITE_UID}/"
-        CLUSTERS="$(api_call ${SITE_API_URI}${CLUSTERS_ENDPOINT})"
+        CLUSTERS="$(get_api_call "${SITE_API_URI}${CLUSTERS_ENDPOINT}")"
         mapfile -t CLUSTERS_UID < <(echo "${CLUSTERS}" | yq '.items[] | .uid')
 
         for CLUSTER_UID in "${CLUSTERS_UID[@]}"; do
-            inventoryLogThis "Processing cluster: ${SITE_UID}/${CLUSTER_UID}" "DEBUG"
-            mkdir -p $INVENTORY_DIRECTORY/$SITE_UID/$CLUSTER_UID/
+            logThis "inventory" "Processing cluster: ${SITE_UID}/${CLUSTER_UID}" "DEBUG" || true
+            mkdir -p "$INVENTORY_DIRECTORY/$SITE_UID/$CLUSTER_UID/"
             
             CLUSTER_API_URI="${SITE_API_URI}${CLUSTERS_ENDPOINT}/${CLUSTER_UID}/"
-            NODES="$(api_call ${CLUSTER_API_URI}${NODES_ENDPOINT})"
+            NODES="$(get_api_call "${CLUSTER_API_URI}${NODES_ENDPOINT}")"
             mapfile -t NODES_UID < <(echo "${NODES}" | yq '.items[] | .uid')
 
 
             for NODE_UID in "${NODES_UID[@]}"; do
-                inventoryLogThis "Processing node: ${SITE_UID}/${CLUSTER_UID}/${NODE_UID}" "DEBUG"
+                logThis "inventory" "Processing node: ${SITE_UID}/${CLUSTER_UID}/${NODE_UID}" "DEBUG" || true
                 NODE_SPECS_FILE_PATH="$INVENTORY_DIRECTORY/$SITE_UID/$CLUSTER_UID/$NODE_UID.json"
                 
-                if ! [[ -f $NODE_SPECS_FILE_PATH ]]; then
-                    echo "$NODE_SPECS_FILE_PATH was too old or not present !"
+                if ! [[ -f "${NODE_SPECS_FILE_PATH}" ]]; then
+                    logThis "inventory" "$NODE_SPECS_FILE_PATH was too old or not present !" "DEBUG" || true
                     NODE_API_URI="${CLUSTER_API_URI}/${NODES_ENDPOINT}/${NODE_UID}${NODE_ENDPOINT_SUFFIX}"
-                    api_call ${NODE_API_URI} > $NODE_SPECS_FILE_PATH
+                    get_api_call "${NODE_API_URI}" > "${NODE_SPECS_FILE_PATH}"
                 else
-                    echo "$NODE_SPECS_FILE_PATH is up to date !"
+                    logThis "inventory" "$NODE_SPECS_FILE_PATH is up to date !" "DEBUG" || true
                 fi
 
                 break
-
             
             done
+            break
         done 
-
+        break
     done
 }
 
+function get_inventory_sites {
+    local INVENTORY_DIRECTORY="$1"
+    local SCRIPT_LOGGING_LEVEL="DEBUG"
+    echo "$(find $INVENTORY_DIRECTORY -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)"
+}
+function get_inventory_site_clusters {
+    local INVENTORY_DIRECTORY="$1"
+    local SITE="$2"
+    local SCRIPT_LOGGING_LEVEL="DEBUG"
+    echo "$(find $INVENTORY_DIRECTORY/$SITE/ -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)"
+}
 
-main
+function get_inventory_site_cluster_nodes {
+    local INVENTORY_DIRECTORY="$1"
+    local SITE="$2"
+    local CLUSTER="$3"
+    local SCRIPT_LOGGING_LEVEL="DEBUG"
+    echo "$(find $INVENTORY_DIRECTORY/$SITE/$CLUSTER/ -maxdepth 1 -mindepth 1 -type f -exec basename {} \;)"
+}
