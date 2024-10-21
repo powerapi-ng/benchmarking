30a16e7e349f63c449057df630365d031126dba1
Inkedstinct <nathan.leblond@inria.fr>
2024-10-21 17:11:24 +0200

    feat(jobs): Add dumb job submission and update/check functions for unfinished jobs

--- libs/jobs.sh
+++ libs/jobs.sh
@@ -11,21 +11,34 @@ function process_jobs {
     rm -f "$JOBS_FIFO"
 }
 
+function submit_job {
+    local WALLTIME="$1"
+    local SITE="$2"
+    local CLUSTER="$3"
+    local NODE="$4"
+    local COMMAND="$5"
+    local RESULT_FILE="$6"
+    local PID="$7"
+    logThis "jobs/submit_job" "Submited : hhs $SITE oarsub -p $NODE -l host=1,walltime=$WALLTIME $COMMAND $RESULT_FILE" "INFO"
+    echo $PID
+}
+
 function add_job {
-    local STATUS="TODO"
+    local STATE="Waiting"
     local JOBS_FILE="$1"
     local ID="$2"
-    local OAR_JOB_ID=""
-    local TASK="$3"
-    local COMMAND="$4"
-    local RESULT_FILE="$5"
-    local METADATA_FILE="$6"
+    local OAR_JOB_ID="$3"
+    local TASK="$4"
+    local COMMAND="$5"
+    local RESULT_FILE="$6"
+    local METADATA_FILE="$7"
 
-    yq -i ".jobs += {\"id\": $(( ID )), \"oar_job_id\": null, \"status\":\"$STATUS\", \"task\":\"$TASK\", \"command\":\"$COMMAND\", \"result_file\":\"$RESULT_FILE\", \"metadata_file\": \"$METADATA_FILE\" }" $JOBS_FILE
+    yq -i ".jobs += {\"id\": $(( ID )), \"oar_job_id\": $(( OAR_JOB_ID )), \"state\":\"$STATE\", \"task\":\"$TASK\", \"command\":\"$COMMAND\", \"result_file\":\"$RESULT_FILE\", \"metadata_file\": \"$METADATA_FILE\" }" $JOBS_FILE
 }
 
 function generate_jobs {
     local JOBS_FILE="$1"
+    local WALLTIME="00:30:00"
     touch $JOBS_FILE
     yq -i '.jobs = []' $JOBS_FILE
     ID=1
@@ -44,7 +57,8 @@ function generate_jobs {
                 RESULT_FILE="./results.d/${SITE}/${CLUSTER}/${NODE_NAME}-${TASK}.json"
                 METADATA_FILE="./inventories.d/${SITE}/${CLUSTER}/${NODE_NAME}.json"
 
-                add_job "$JOBS_FILE" "$ID" "$TASK" "$COMMAND" "$RESULT_FILE" "$METADATA_FILE"
+                OAR_JOB_ID="$(submit_job "$WALLTIME" "$SITE" "$CLUSTER" "$NODE_NAME" "$COMMAND" "$RESULT_FILE" "$ID")"
+                add_job "$JOBS_FILE" "$ID" "$OAR_JOB_ID" "$TASK" "$COMMAND" "$RESULT_FILE" "$METADATA_FILE"
 
                 ID=$(( ID + 1 ))
                 #echo "  PROCESSOR model : $(yq '.processor.model' ./inventories.d/$SITE/$CLUSTER/$NODE)"
@@ -54,4 +68,40 @@ function generate_jobs {
             done
         done
     done
+}
+
+function check_on_unfinished_jobs {
+    local JOBS_FILE="$1"
+    
+    local WAITING_JOBS="$(yq -e '.jobs[] | select(.state == "Waiting") | .oar_job_id' $JOBS_FILE)"
+
+    for WAITING_JOB in ${WAITING_JOBS[@]}; do
+        CURRENT_STATE="$(ssh lille oarstat -f -j $WAITING_JOB | grep 'state = ' | awk -F' ' '{print $3 }')"
+        if [[ "$CURRENT_STATE" == "Waiting" ]]; then
+            logThis "jobs/check_on_unfinished_jobs" "Job $WAITING_JOB still waiting" "DEBUG"
+        else
+            yq -i ".jobs[] | select(.oar_job_id == $(( WAITING_JOB ))) | .state = $CURRENT_STATE" $JOBS_FILE
+        fi
+    done
+
+    local RUNNING_JOBS="$(yq -e '.jobs[] | select(.state == "Running") | .oar_job_id' $JOBS_FILE)"
+
+    for RUNNING_JOB in ${RUNNING_JOBS[@]}; do
+        CURRENT_STATE="$(ssh lille oarstat -f -j $RUNNING_JOB | grep 'state = ' | awk -F' ' '{print $3 }')"
+        if [[ "$CURRENT_STATE" == "Running" ]]; then
+            logThis "jobs/check_on_unfinished_jobs" "Job $RUNNING_JOB still running" "DEBUG"
+        else
+            yq -i ".jobs[] | select(.oar_job_id == $(( RUNNING_JOB ))) | .state = $CURRENT_STATE" $JOBS_FILE
+        fi
+    done
+}
+
+function job_is_done {
+    local JOBS_FILE="$1"
+
+    if [[ "$(yq -e '[.jobs[] | select(.status == "Waiting" or "Running")] | length' $JOBS_FILE)" == "0" ]]; then
+        return 0
+    else
+        return 1
+    fi
 }
