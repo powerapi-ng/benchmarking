777eabe3763aee0369f05ac5d160a20be008562a
Inkedstinct <nathan.leblond@inria.fr>
2024-11-18 09:29:40 +0100

    feat(results): Ensure extracted archives only contains needed directory

--- templates/zip_results.sh
+++ templates/zip_results.sh
@@ -1,2 +1,5 @@
-tar -cJf {{ results_directory }}.tar.xz {{ results_directory }} && rm -rf {{ results_directory }}
+cd {{ results_directory }}/..
+tar -cJf ~/{{ results_directory }}.tar.xz ~/{{ results_directory }}
+cd ~
+rm -rf {{ results_directory }}
 md5sum {{ results_directory }}.tar.xz  > {{ results_directory }}.tar.xz.md5
