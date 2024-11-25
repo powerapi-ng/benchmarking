56638ff158feba401d1d0634312f44910330b657
Inkedstinct <nathan.leblond@inria.fr>
2024-11-25 17:18:01 +0100

    feat(results) : Add perf aggregation from file and hwpc from main dir

--- src/results.rs
+++ src/results.rs
@@ -1,10 +1,246 @@
 use thiserror::Error;
+use log::{debug, info, warn};
+use serde::{Serialize, Deserialize};
+use csv::{Reader};
+use std::path::Path;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Write};
+use std::fs;
+use std::collections::HashMap;
+use plotters::prelude::*;
 
 #[derive(Error, Debug)]
-pub enum ResultError {}
+pub enum ResultError {
+    #[error("Could not parse CSV File : {0}")]
+    Csv(#[from] csv::Error),
+}
 
-#[derive()]
-pub struct HwpcReport {}
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+struct HwpcRowRaw {
+    timestamp: i64,
+    sensor: String,
+    target: String,
+    socket: i32,
+    cpu: i32,
+    RAPL_ENERGY_PKG: Option<i64>,
+    RAPL_ENERGY_DRAM: Option<i64>,
+    RAPL_ENERGY_CORES: Option<i64>,
+    time_enabled: i64,
+    time_running: i64,
+}
 
-#[derive()]
-pub struct PerfReport {}
+
+
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+struct HwpcRow {
+    timestamp: i64,
+    sensor: String,
+    target: String,
+    socket: i32,
+    cpu: i32,
+    rapl_energy_pkg: Option<i64>,
+    rapl_energy_dram: Option<i64>,
+    rapl_energy_cores: Option<i64>,
+    time_enabled: i64,
+    time_running: i64,
+    nb_core: i32,
+    nb_ops_per_core: i32,
+    iteration: usize
+}
+
+impl HwpcRow {
+    fn from_raw_record(raw_record: HwpcRowRaw, nb_core: i32, nb_ops_per_core: i32, iteration: usize) -> Self {
+        HwpcRow {
+            timestamp: raw_record.timestamp,
+            sensor: raw_record.sensor,
+            target: raw_record.target,
+            socket: raw_record.socket,
+            cpu: raw_record.cpu,
+            rapl_energy_pkg: raw_record.RAPL_ENERGY_PKG,
+            rapl_energy_dram: raw_record.RAPL_ENERGY_DRAM,
+            rapl_energy_cores: raw_record.RAPL_ENERGY_CORES,
+            time_enabled: raw_record.time_enabled,
+            time_running: raw_record.time_running,
+            nb_core,
+            nb_ops_per_core,
+            iteration
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+struct PerfRow {
+    power_energy_pkg: Option<f64>,
+    power_energy_ram: Option<f64>,
+    power_energy_cores: Option<f64>,
+    time_elapsed: f64,
+    nb_core: i32,
+    nb_ops_per_core: i32,
+    iteration: usize,
+}
+
+#[derive(Debug)]
+struct Statistics {
+    nb_core: i32,
+    nb_ops_per_core: i32,
+    task: String,
+    mean: f64,
+    variance: f64,
+    std_dev: f64,
+    coeff_variation: f64,
+}
+
+/// Creates an aggregation of perf_<KIND>_<NB_CPU>_<NB_OPS_PER_CPU> into corresponding perf_alone_<NB_CPU>_<NB_OPS_PER_CPU>.csv file
+fn aggregate_perf(raw_perf_results_file: &str) -> io::Result<()> {
+    debug!("Processing perf file '{}'", raw_perf_results_file);
+
+    let output_path = &format!("{}.csv", raw_perf_results_file);
+    fs::File::create(output_path)?;
+    let mut output_writer = csv::Writer::from_path(output_path)?;
+
+    if let Some((nb_core, nb_ops_per_core)) = parse_perf_metadata(raw_perf_results_file) {
+        let raw_perf_results_file = File::open(raw_perf_results_file)?;
+        let reader = BufReader::new(raw_perf_results_file);
+        let mut iteration = 1;
+        let mut cores_joules = None;
+        let mut pkg_joules = None;
+        let mut ram_joules = None;
+        let mut time_elapsed = None;
+
+        for line in reader.lines() {
+            let line = line?;
+            if line.contains("power/energy-cores/") {
+                if let Some(value) = line.trim().split_whitespace().next() {
+                    cores_joules = Some(value.parse::<f64>().unwrap_or_default());
+                }
+            } else if line.contains("power/energy-pkg/") {
+                if let Some(value) = line.trim().split_whitespace().next() {
+                    pkg_joules = Some(value.parse::<f64>().unwrap_or_default());
+                }
+            } else if line.contains("power/energy-ram/") {
+                if let Some(value) = line.trim().split_whitespace().next() {
+                    ram_joules = Some(value.parse::<f64>().unwrap_or_default());
+                }
+            } else if line.contains("seconds time elapsed") {
+                if let Some(value) = line.trim().split_whitespace().next() {
+                    time_elapsed = Some(value.parse::<f64>().unwrap_or_default());
+                }
+                let perf_row = PerfRow {
+                    power_energy_pkg: pkg_joules,
+                    power_energy_ram: ram_joules,
+                    power_energy_cores: cores_joules,
+                    time_elapsed: time_elapsed.unwrap(),
+                    nb_core: nb_core.parse::<i32>().unwrap(),
+                    nb_ops_per_core: nb_ops_per_core.parse::<i32>().unwrap(),
+                    iteration
+                };
+                output_writer.serialize(perf_row)?;
+                iteration += 1;
+                cores_joules = None;
+                pkg_joules = None;
+                ram_joules = None;
+                time_elapsed = None; // Reset for the next iteration
+            }
+        }
+    } else {
+        warn!("Could not parse metadata from file name: {:?}", raw_perf_results_file);
+    }
+
+    Ok(())
+}
+
+fn parse_perf_metadata(file_name: &str) -> Option<(String, String)> {
+    if let Some(file_name) = Path::new(file_name).file_name().and_then(|os_str| os_str.to_str()) {
+        let parts: Vec<&str> = file_name.split('_').collect();
+        if parts.len() == 4 {
+            if let (Ok(nb_core), Ok(nb_ops_per_core)) = (parts[2].parse::<u32>(), parts[3].parse::<u32>()) {
+                return Some((nb_core.to_string(), nb_ops_per_core.to_string()));
+            }
+        } else if parts.len() == 5 {
+            if let (Ok(nb_core), Ok(nb_ops_per_core)) = (parts[3].parse::<u32>(), parts[4].parse::<u32>()) {
+                return Some((nb_core.to_string(), nb_ops_per_core.to_string()));
+            }
+        }
+    } else {
+        warn!("Could not parse filename {} to get metadata", file_name);
+    }
+    None
+}
+
+fn parse_hwpc_metadata(dir_name: &str) -> Option<(i32, i32, usize)> {
+    if let Some(dir_name) = Path::new(dir_name).file_name().and_then(|os_str| os_str.to_str()) {
+        let parts: Vec<&str> = dir_name.split('_').collect();
+        if parts.len() == 5 {
+            if let (Ok(nb_core), Ok(nb_ops_per_core), Ok(iteration)) = (parts[2].parse::<i32>(), parts[3].parse::<i32>(), parts[4].parse::<usize>()) {
+                return Some((nb_core, nb_ops_per_core, iteration));
+            }
+        } else if parts.len() == 6 {
+            if let (Ok(nb_core), Ok(nb_ops_per_core), Ok(iteration)) = (parts[3].parse::<i32>(), parts[4].parse::<i32>(), parts[5].parse::<usize>()) {
+                return Some((nb_core, nb_ops_per_core, iteration));
+            }
+        }
+    } else {
+        warn!("Could not parse filename {} to get metadata", dir_name);
+    }
+    None
+}
+
+fn aggregate_hwpc_file(raw_rapl_file: &Path, output_path: &str, nb_core: i32, nb_ops_per_core: i32, iteration: usize) -> io::Result<()> {
+    let mut output_writer = csv::Writer::from_path(output_path)?;
+    let mut reader = csv::Reader::from_path(raw_rapl_file)?;
+    let iter  = reader.deserialize::<HwpcRowRaw>();
+
+    for hwpc_row_raw in iter {
+        let hwpc_raw = HwpcRow::from_raw_record(hwpc_row_raw?, nb_core, nb_ops_per_core, iteration);
+        output_writer.serialize(hwpc_raw)?;
+    }
+    Ok(())
+}
+
+fn aggregate_hwpc_subdir(subdir: &fs::DirEntry, output_path: &str) -> io::Result<()> {
+
+    let raw_rapl_file = subdir.path().join("rapl.csv");
+    info!("Processing hwpc file {:?}", raw_rapl_file);
+    if let Some((nb_core, nb_ops_per_core, iteration)) = parse_hwpc_metadata(subdir.path().to_str().unwrap()) {
+        aggregate_hwpc_file(&raw_rapl_file, output_path, nb_core, nb_ops_per_core, iteration)?;
+    } else {
+        warn!("Could not parse metadata from directory name: {:?}", subdir);
+    }
+    Ok(())
+}
+
+/// Creates an aggregation of hwpc_<KIND>_<NB_CPU>_<NB_OPS_PER_CPU> into corresponding hwpc_<KIND>_<NB_CPU>_<NB_OPS_PER_CPU>.csv file
+fn aggregate_hwpc(
+    raw_results_dir: &str,
+) -> io::Result<()> {
+    
+    let raw_results_dir_path = Path::new(raw_results_dir);
+    let (output_parent, output_basename) = (raw_results_dir_path.parent().unwrap(), raw_results_dir_path.file_name().unwrap());
+    let output_path = &format!("{}/{}.csv", output_parent.to_str().unwrap(), output_basename.to_str().unwrap());
+
+
+    let mut raw_results_subdirs = Vec::new();
+
+    if let Ok(entries) = fs::read_dir(raw_results_dir) {
+        raw_results_subdirs = entries.filter(|entry| entry.as_ref().unwrap().file_type().unwrap().is_dir()).collect();
+    } else {
+        warn!("Could not find subdirectories in {} directory", raw_results_dir);
+    }
+
+    debug!("Found {:?} subdirs", raw_results_subdirs);
+    assert!(raw_results_subdirs.iter().map(|subdir| aggregate_hwpc_subdir(subdir.as_ref().unwrap(), output_path)).all(|result| result.is_ok()));
+
+    Ok(())
+}
+
+pub fn process_results() -> io::Result<()> {
+    let perf_raw_results_file = "test_results/lille/chiclet/chiclet-3/perf_alone_18_25";
+    aggregate_perf(perf_raw_results_file)?;
+
+    let hwpc_raw_results_dir = "test_results/lille/chiclet/chiclet-3/hwpc_alone_18_25";
+    aggregate_hwpc(hwpc_raw_results_dir)?;
+
+    Ok(())
+}
+
