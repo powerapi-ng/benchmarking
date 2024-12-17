4862e4543b4d326df806ff671899d2df738dac78
Inkedstinct <nathan.leblond@inria.fr>
2024-12-17 15:08:52 +0100

    feat(templates) : Add manual docker install in case of none-default env + avoid sudo as user is sudo

--- templates/install_packages.sh
+++ templates/install_packages.sh
@@ -1,7 +1,21 @@
-sudo-g5k apt-get install -y stress-ng
+{% if os_flavor != super::DEFAULT_OS_FLAVOR %}
+    curl -sSL https://get.docker.com/ | sh
+    sudo curl -sSL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo mkdir -p /etc/docker
+    echo "{ \"registry-mirrors\": [\"http://docker-cache.grid5000.fr\"] }" | sudo tee /etc/docker/daemon.json
+    sudo systemctl restart docker
+    sudo chmod o+rw /var/run/docker.sock
+    SUDO_CMD=""
+{% else %}
+    g5k-setup-docker
+    SUDO_CMD="sudo-g5k "
+{% endif %}
 
-sudo rm -f /etc/apt/sources.list.d/repo.radeon.com-amdgpu.list
-g5k-setup-docker
+
+${SUDO_CMD}apt-get install -y stress-ng
+
+${SUDO_CMD}rm -f /etc/apt/sources.list.d/repo.radeon.com-amdgpu.list
 docker login -u {{ docker_hub_username }} -p {{ docker_hub_token }}
 docker run --rm -d --name mongo -p 27017:27017 mongo:latest
 sleep 30
--- templates/oar_directives.sh
+++ templates/oar_directives.sh
@@ -1,6 +1,8 @@
-#OAR -q {{ queue_type }}
-#OAR -p {{ node_uid }}
-#OAR -l host=1,walltime={{ walltime }}
-{% if exotic_node %}
-#OAR -t exotic
+{% if os_flavor == super::DEFAULT_OS_FLAVOR %}
+    #OAR -q {{ queue_type }}
+    #OAR -p {{ node_uid }}
+    #OAR -l host=1,walltime={{ walltime }}
+    {% if exotic_node %}
+    #OAR -t exotic
+    {% endif %}
 {% endif %}
